c1b40b4ba5a4a4c43c0f82d3958ff371f3f104e0
Mike Heffner <mikeh@fesnel.com>
2013-05-04 00:00:42 -0400

    Switch to publishing with librato-metrics.

--- lib/librato/metrics/taps/publisher.rb
+++ lib/librato/metrics/taps/publisher.rb
@@ -1,98 +1,85 @@
 module Librato
   module Metrics
     module Taps
-      module Publisher
-        class << self
-          SOURCE_NAME_REGEX = /^[-A-Za-z0-9_.]{1,255}$/
-          DEFAULT_URL = 'https://metrics-stg.heroku.com'
+      class Publisher
+        SOURCE_NAME_REGEX = /^[-A-Za-z0-9_.]{1,255}$/
+        DEFAULT_URL = 'https://metrics-stg.heroku.com'
 
-          @url = nil
-          @user = nil
-          @passwd = nil
+        @url = nil
+        @user = nil
+        @passwd = nil
 
-          def url=(new_url)
-            if new_url =~ /^http/
-              @url = new_url
-            else
-              @url = "https://#{new_url}"
+        DEFAULT_BATCH_SIZE = 300
+
+        def initialize(opts)
+          unless !opts[:source] || opts[:source] =~ SOURCE_NAME_REGEX
+            raise "Invalid source"
+          end
+
+          unless !opts[:prefix] || opts[:prefix] =~ SOURCE_NAME_REGEX
+            raise "Invalid prefix"
+          end
+
+          @client = Librato::Metrics::Client.new
+          @client.authenticate(opts[:email], opts[:token])
+          @client.api_endpoint = opts[:metrics_url]
+
+          qparams = {
+            :autosubmit_count => DEFAULT_BATCH_SIZE
+          }
+
+          [:prefix, :source].each do |o|
+            qparams[o] = opts[o] if opts[o]
+          end
+
+          @queue_params = qparams
+        end
+
+        # Post metrics to metrics service
+        #
+        #  counters:
+        #    {:my-counter-1 => 4, :my-counter-2 => 6, ...}
+        #
+        #  gauges:
+        #    {:my-gauge-1 => 4, ...}
+        #    or
+        #    {:my-gauge-2 => {:value => 5, :count => 4,
+        #                     :min => 3, :max => 6, ...}}
+        #    or mix-match.
+        def post(counters, gauges, params = {})
+          mt = params[:measure_time] || now_secs
+          q = @client.new_queue(@queue_params)
+
+          if counters.length > 0
+            counters.each_pair do |k, v|
+              q.add k => {:type => :counter, :value => v, :measure_time => mt}
             end
           end
 
-          def url
-            @url || DEFAULT_URL
-          end
-
-          def user=(new_user)
-            @user = CGI.escape(new_user)
-          end
-
-          def passwd=(new_passwd)
-            @passwd = new_passwd
-          end
-
-          def build_url(path)
-            fullurl = url
-            return fullurl+path unless @user && @passwd
-
-            fullurl.gsub(/(http(s|):\/\/)/, "\\1#{@user}:#{@passwd}@") + path
-          end
-
-          # Post metrics to metrics service
-          #
-          #  counters:
-          #    {:my-counter-1 => 4, :my-counter-2 => 6, ...}
-          #
-          #  gauges:
-          #    {:my-gauge-1 => 4, ...}
-          #    or
-          #    {:my-gauge-2 => {:value => 5, :count => 4,
-          #                     :min => 3, :max => 6, ...}}
-          #    or mix-match.
-          def post(counters, gauges, params = {})
-            unless !params[:source] || params[:source] =~ SOURCE_NAME_REGEX
-              raise "Invalid source"
-            end
-
-            unless !params[:prefix] || params[:prefix] =~ SOURCE_NAME_REGEX
-              raise "Invalid prefix"
-            end
-
-            pfx = ""
-            if params[:prefix]
-              pfx = params.delete(:prefix) + "."
-            end
-
-            if counters.length > 0
-              params[:counters] = {}
-              counters.each_pair do |k, v|
-                k = pfx + k
-                params[:counters][k] = {:value => v}
+          if gauges.length > 0
+            params[:gauges] = {}
+            gauges.each_pair do |k, v|
+              if v.respond_to?(:keys)
+                q.add k => v.merge(:measure_time => mt)
+              else
+                q.add k => {:value => Float(v), :measure_time => mt}
               end
             end
-            if gauges.length > 0
-              params[:gauges] = {}
-              gauges.each_pair do |k, v|
-                k = pfx + k
-                if v.respond_to?(:keys)
-                  params[:gauges][k] = v
-                else
-                  params[:gauges][k] = {:value => Float(v)}
-                end
-              end
-            end
-
-            begin
-              r = RestClient.post build_url('/v1/metrics.json'), params
-            rescue RestClient::Exception => err
-              puts "Failed to publish stats: #{err.message}"
-              puts "Response: #{err.response}"
-              return false
-            rescue => err
-              puts "Failed to publish stats (unknown exception): #{err.inspect}"
-              return false
-            end
-            true
           end
+
+          begin
+            q.submit
+          rescue => err
+            # XXX: ever get here? what about auto-submit?
+            puts "Failed to publish stats (unknown exception): #{err.inspect}"
+            return false
+          end
+
+          true
+        end
+
+        def now_secs
+          Time.now.tv_sec
         end
       end
     end
